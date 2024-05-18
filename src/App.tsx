49dc638c5eb182fbb23d7c3bcdfcc80af3e6a90b
mathi669 <matias.luarte@hotmail.com>
2024-05-17 23:34:36 -0400

    add login user test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import NavbarMartial from './components/Nav'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Suspense } from 'react';
 import { AppRouter } from './Router';
 import Footer from './components/Footer';
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../static/css/main.css";
+import { useState, ChangeEvent, FormEvent } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login: React.FC = () => {
+  const [userType, setUserType] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const navigate = useNavigate()
+
+  const handleUserTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setUserType(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .post("http://127.0.0.1:5000/login", { email, password })
+      .then((res) => {
+        console.log(res)
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <section className="full-width section">
       <div className="container">
@@ -14,91 +37,92 @@ const Login = () => {
               ></i>
               <h4 className="text-center text-light">INICIAR SESIÓN</h4>
               <br />
-              <form id="loginForm" action="" method="post">
-                {/* Contenedor para seleccionar el tipo de inicio de sesión */}
+              <form id="loginForm" onSubmit={handleSubmit}>
                 <div className="form-group">
                   <select
                     className="form-control input-lg"
                     id="userType"
                     name="userType"
+                    value={userType}
+                    onChange={handleUserTypeChange}
                   >
-                    <option value="">
-                      Seleccione tipo de inicio de sesión
-                    </option>
+                    <option value="">Seleccione tipo de inicio de sesión</option>
                     <option value="usuario">Iniciar sesión como usuario</option>
-                    <option value="gimnasio">
-                      Iniciar sesión como gimnasio
-                    </option>
+                    <option value="gimnasio">Iniciar sesión como gimnasio</option>
                   </select>
                 </div>
 
-                {/* Formulario para iniciar sesión como usuario */}
-                <div id="usuarioForm" style={{ display: "none" }}>
-                  <div className="form-group">
-                    <input
-                      type="email"
-                      className="form-control input-lg"
-                      name="emailUsuario"
-                      placeholder="Correo electrónico"
-                      required
-                    />
+                {userType === "usuario" && (
+                  <div id="usuarioForm">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        className="form-control input-lg"
+                        name="emailUsuario"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="password"
+                        className="form-control input-lg"
+                        name="passwordUsuario"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
                   </div>
-                  <div className="form-group">
-                    <input
-                      type="password"
-                      className="form-control input-lg"
-                      name="passwordUsuario"
-                      placeholder="Contraseña"
-                      required
-                    />
-                  </div>
-                </div>
+                )}
 
-                {/* Formulario para iniciar sesión como gimnasio */}
-                <div id="gimnasioForm" style={{ display: "none" }}>
-                  <div className="form-group">
-                    <input
-                      type="email"
-                      className="form-control input-lg"
-                      name="emailGimnasio"
-                      placeholder="Correo electrónico"
-                      required
-                    />
+                {userType === "gimnasio" && (
+                  <div id="gimnasioForm">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        className="form-control input-lg"
+                        name="emailGimnasio"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="password"
+                        className="form-control input-lg"
+                        name="passwordGimnasio"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
                   </div>
-                  <div className="form-group">
-                    <input
-                      type="password"
-                      className="form-control input-lg"
-                      name="passwordGimnasio"
-                      placeholder="Contraseña"
-                      required
-                    />
-                  </div>
-                </div>
+                )}
 
                 <button className="btn btn-danger btn-lg" type="submit">
                   INICIAR SESIÓN
                 </button>
               </form>
 
-              {/* Enlaces adicionales */}
               <div className="text-left text-light">
                 <a href="#!">No recuerdo mi contraseña</a>
               </div>
 
               <div className="checkbox full-width" style={{ margin: "20px 0" }}>
                 <label>
-                  {" "}
-                  <input type="checkbox" /> No cerrar sesión{" "}
+                  <input type="checkbox" /> No cerrar sesión
                 </label>
               </div>
 
               <div className="text-center">
-                <a>
-                    <Link to="registro">
-                        Si eres nuevo ¡Crea una cuenta!
-                    </Link>
-                </a>
+                <Link to="registro">Si eres nuevo ¡Crea una cuenta!</Link>
               </div>
             </div>
           </div>
