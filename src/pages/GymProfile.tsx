872ce40aee7e6d35562170db5860614efb486add
Raffaelsrc <ra.rodriguezc@duocuc.cl>
2024-06-07 22:43:05 -0400

    cambios en la pagina gymprofile

--- src/pages/GymProfile.tsx
+++ src/pages/GymProfile.tsx
@@ -1,167 +1,67 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import "../static/css/main.css";
+import {
+  Box,
+  Button,
+  VStack,
+  HStack,
+  Text,
+  Avatar,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
+import { FaCheckCircle } from "react-icons/fa";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const GymProfile = () => {
   return (
-    <section className="full-width section">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-sm-4 col-md-3">
-            <button
-              className="btn btn-default btn-block visible-xs btn-dropdown-container"
-              data-drop-cont=".user-menu-xs"
-            >
-              <i className="fa fa-user fa-fw" aria-hidden="true"></i> MOSTRAR
-              MENÚ <i className="fa fa-sort pull-right" aria-hidden="true"></i>
-            </button>
-            <div className="full-width post-user-info">
-              <img
-                src="./src/static/img/user.png"
-                className="NavBar-Nav-icon"
-                alt="User"
-              />
-              <p className="full-width">
-                <small id="nombreGimnasio"></small>
-              </p>
-              <div className="full-width div-table">
-                <div className="full-width div-table-row">
-                  <div
-                    className="div-table-cell div-table-cell-xs"
-                    id="ubicacionGimnasio"
-                  >
-                    Ubicación <br />
-                    <small>Ciudad, País</small>
-                  </div>
-                  <div
-                    className="div-table-cell div-table-cell-xs"
-                    id="telefonoGimnasio"
-                  >
-                    Teléfono <br />
-                    <small>Número de contacto</small>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="full-width list-group"
-                style={{ borderRadius: "0" }}
-              >
-                <div className="list-group-item text-center">
-                  <small>Desde Fecha de Registro</small>
-                </div>
-                <a className="list-group-item active">
-                    <Link to="/profile">
-                        <i className="fa fa-user fa-fw" aria-hidden="true"></i> TU
-                        PERFIL
-                    
-                    </Link>
-                </a>
-                <a className="list-group-item">
-                    <Link to="/adminPage">
-                        <i className="fa fa-cogs fa-fw" aria-hidden="true"></i>{" "}
-                        CONFIGURACIÓN
-                    </Link>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-8 col-md-9">
-            <div className="full-width bar-info-user">
-              <i className="fa fa-user fa-fw" aria-hidden="true"></i>
-              <div>TU PERFIL</div>
-            </div>
-            {/* Contenido*/}
-            <div
-              className="full-width"
-              style={{ padding: "15px", border: "1px solid #E1E1E1" }}
-            >
-              <form action="">
-                <p className="text-muted text-center">Seleccione una imagen</p>
-                <div className="form-group">
-                  <div className="custom-input-file">
-                    <input type="file"  className="input-file" />
-                    <i className="fa fa-picture-o" aria-hidden="true"></i>
-                  </div>
-                  <br />
-                  <p className="text-muted text-center archivo">Archivo...</p>
-                </div>
-                <br />
-                <br />
-                <br />
-                <div className="form-group">
-                  <label>Nombre del Gimnasio</label>
-                  <input
-                    type="text"
-                    placeholder="Nombre del Gimnasio"
-                    className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>
-                    Teléfono <small></small>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="¿Cuál es tu teléfono?"
-                    className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>
-                    Ubicación <small>¿Cuál es tu ubicación?</small>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Ubicación"
-                    className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Correo electrónico</label>
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Contraseña</label>
-                  <a
-                    href="#!"
-                    className="btn btn-default btn-xs pull-right btn-dropdown-container"
-                    data-drop-cont=".perfil-password"
-                  >
-                    Mostrar/Ocultar{" "}
-                    <i className="fa fa-sort" aria-hidden="true"></i>
-                  </a>
-                  <div className="full-width perfil-password">
-                    <input
-                      type="password"
-                      placeholder="Contraseña"
-                      className="form-control"
-                    />
-                    <br />
-                    <input
-                      type="password"
-                      placeholder="Nueva Contraseña"
-                      className="form-control"
-                    />
-                    <br />
-                    <input
-                      type="password"
-                      placeholder="Confirmar Contraseña"
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-                <p className="text-center">
-                  <button className="btn btn-danger">GUARDAR</button>
-                </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Box p={4}>
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem colSpan={5}>
+          <HStack spacing={4} alignItems="center">
+            <Avatar size="xl" name="Ignatius Keller" src="path/to/avatar.jpg" />
+            <VStack align="start">
+              <HStack>
+                <Text fontSize="2xl" fontWeight="bold">Ignatius Keller</Text>
+                <FaCheckCircle color="blue" />
+              </HStack>
+              <Text>Product Manager</Text>
+              <Text>Sausalito, California, United States</Text>
+              <Text>376 connect</Text>
+            </VStack>
+          </HStack>
+          <HStack mt={4} spacing={2}>
+            <Button size="sm">Open to</Button>
+            <Button size="sm">Add profile section</Button>
+            <Button size="sm">More</Button>
+            <Button size="sm">Disciplinas</Button>
+            <Button size="sm">Cancelar clase</Button>
+            <Button size="sm">Revisar horario</Button>
+          </HStack>
+        </GridItem>
+        <GridItem colSpan={4}>
+          <Box mt={4} p={4} borderWidth="1px" borderRadius="md">
+            <Text fontWeight="bold" mb={2}>Actividad reciente</Text>
+            <Box borderWidth="1px" borderRadius="md" p={4} mb={4}>
+              <Text>Actividad 1</Text>
+              <Button size="sm" mt={2}>Modificar</Button>
+            </Box>
+            <Box borderWidth="1px" borderRadius="md" p={4}>
+              <Text>Actividad 2</Text>
+              <Button size="sm" mt={2}>Modificar</Button>
+            </Box>
+          </Box>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Box mt={4} p={4} borderWidth="1px" borderRadius="md">
+            <Text fontWeight="bold" mb={4}>Horarios:</Text>
+            <Calendar />
+            <Button size="sm" mt={4}>Agendar clase</Button>
+          </Box>
+        </GridItem>
+      </Grid>
+    </Box>
   );
 };
 
